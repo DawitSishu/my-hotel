d65cd0b76953a4b875c2315cd5de71a463129c1b
DawitSishu <dawitalemayhu55@gmail.com>
2023-06-03 04:04:45 -0400

    basic boiler platefor login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 //make  user signup to reserve room  || show a dummy payment screen
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { useState } from 'react';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
+import Home from './components/Home/Home';
 
 function App() {
+  const [loggedIn, SetLoggedIn] = useState(false);
+  const handleLogIn = () =>{
+      SetLoggedIn(true)
+  } 
+  const handleLogOut = () =>{
+      SetLoggedIn(false)
+  } 
+
+
   return (
-    <div className="App">
-      hi
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route exact path="/" element={loggedIn ? <Home onLogOut={handleLogOut}/> : <Login onLogIn = {handleLogIn} />}/>
+        <Route path='/signup' element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
