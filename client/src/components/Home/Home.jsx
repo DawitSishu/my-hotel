a9833cc40e86dbdfe022864f2b674dcd5630c284
DawitSishu <dawitalemayhu55@gmail.com>
2023-06-05 04:30:03 -0400

    Home page basic done

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
 import background from "../../Assets/homevid.mp4";
 import '../Login/Login.css';
+import './Home.css';
+import HomeBody from "./HomeBody";
+
 const Home = (props) => {
+  
   return (
     <div className="main">
         <video src={background} autoPlay muted loop />
+        <div className="overlay"></div>
+        <HomeBody />
     </div>
   )
 }
---
+++ client/src/components/Home/HomeBody.jsx
@@ -0,0 +1,35 @@
+import { Box,Typography,Grid } from "@mui/material";
+import MouseIcon from '@mui/icons-material/Mouse';
+const HomeBody = () => {
+  return (
+    <Box
+          sx={{
+            display: 'flex',
+      alignItems: 'center',
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      height:'100vh',
+          }}
+        >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+        <Typography variant="h1" sx={{ fontFamily: 'myCursive',  color: 'white' }}>My Hotel</Typography>
+          </Grid>
+          <Grid item xs={12}>
+             <MouseIcon sx={{
+                color:'white', 
+                '@media (max-width: 800px)': {
+                     display: 'none',
+                    },
+                }}  
+                className="scroll-icon"
+              />
+          </Grid>
+        </Grid>
+        </Box>
+  )
+}
+
+export default HomeBody;
