df094635ea0462ebeaed0d26fd8116137290ad18
DawitSishu <dawitalemayhu55@gmail.com>
2023-06-14 03:30:05 -0400

    rooms setup

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -1,6 +1,10 @@
 import {useState,useEffect,useRef} from 'react';
 import { useNavigate,useLocation } from 'react-router-dom';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles'; 
+import {Typography,Box,Grid} from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import NavBar from '../NavBar/NavBar';
+import photo from '../../Assets/9.jpg';
 
 
 const Rooms = () => {
@@ -8,6 +12,12 @@ const Rooms = () => {
     const navigate = useNavigate();
     const location = useLocation();
     
+
+    const darkTheme = createTheme({
+      palette: {
+        mode: 'dark',
+      },
+    });
     
     useEffect(()=>{
         const availableRoooms  = location.state;
@@ -16,6 +26,7 @@ const Rooms = () => {
             navigate('/');
         }else{
             // setRooms(rooms)
+            console.log(availableRoooms);
             setRooms(availableRoooms);
         }
     },[])
@@ -24,7 +35,43 @@ const Rooms = () => {
 
 
   return (
-    <div>Rooms</div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <NavBar />
+      <Grid
+      container 
+      spacing={2}
+      width='100vw'
+      >
+        <Grid 
+          item 
+          xs={12} 
+          sx ={{
+            marginLeft:5,
+            marginTop: 12,
+          }}
+          >
+              <Box>
+                <img src={photo} style={{maxWidth:300}} />
+              </Box>
+            </Grid>
+      </Grid>
+      {/* <Grid 
+        container 
+        spacing={2}
+        sx={{
+          marginLeft:5,
+          marginTop: 10,
+          overflowX: 'hidden',
+        }}
+        >
+            <Grid item xs={12}>
+              <Box>
+                <img src={photo} style={{maxWidth:300}} />
+              </Box>
+            </Grid>
+      </Grid> */}
+    </ThemeProvider>
   )
 }
 
